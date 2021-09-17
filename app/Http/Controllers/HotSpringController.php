b8f45e2c08c6ee8087e2ba75b3bb363d7025ffb3
YASUO0715 <yasuo0715niihori@gmail.com>
2021-09-17 21:23:26 +0900

    温泉再修正5

--- app/Http/Controllers/HotSpringController.php
+++ app/Http/Controllers/HotSpringController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class WifiSpotController extends Controller
 {
-    public function index()
+    public function hotspring()
     {
         return view('hot-spring.hotspring');
     }
