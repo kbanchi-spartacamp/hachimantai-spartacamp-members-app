70e86d53dc0ca551e6379eaace261f40026c5bd8
YASUO0715 <yasuo0715niihori@gmail.com>
2021-09-17 21:07:05 +0900

    八幡平市の温泉のページを編集2

--- app/Http/Controllers/HotSpringController.php
+++ app/Http/Controllers/HotSpringController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HotSpringController extends Controller
 {
-    public function freeword($hot_spring)
+    public function hot_spring($hot_spring)
     {
         return view('hotspring', [
             'hot_spring' => $hot_spring
