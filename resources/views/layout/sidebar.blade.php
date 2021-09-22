eece5d4e664b64627dd4f3b4d597c4253fd0099b
kbanchi1111 <kbanchi.1111@gmail.com>
2021-09-22 22:57:57 +0900

    update sidebar and wifi

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -9,6 +9,30 @@
                     <a class="nav-link" href="/hot-spring">
                         温泉
                     </a>
+                    <a class="nav-link" href="/recruits">
+                        求人・リクルート
+                    </a>
+                    <a class="nav-link" href="/restaurants">
+                        飲食店
+                    </a>
+                    <a class="nav-link" href="/grocery-store">
+                        スーパー
+                    </a>
+                    <a class="nav-link" href="/laundry">
+                        コインランドリー
+                    </a>
+                    <a class="nav-link" href="/gas-station">
+                        ガソリンスタンド
+                    </a>
+                    <a class="nav-link" href="/morioka-info">
+                        盛岡駅
+                    </a>
+                    <a class="nav-link" href="/car">
+                        車・レンタカー
+                    </a>
+                    <a class="nav-link" href="/sightseeing">
+                        観光地
+                    </a>
                 </li>
             </ul>
         </div>
--- resources/views/wifi-spot/wifi-spot.blade.php
+++ resources/views/wifi-spot/wifi-spot.blade.php
@@ -4,27 +4,28 @@
 @include('layout.header')
 
 @section('content')
-    <div class="album py-5 bg-light">
-        <div class="container">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                @foreach ($wifispots as $wifispot)
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <h4>{{ $wifispot->name }}</h4>
-                            <img src="{{ $wifispot->image_url }}" alt="Sa" width="100%" height="225">
-                            <div class="card-body">
-                                <p class="card-text">{{ $wifispot->description }}</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <a href="{{ $wifispot->hp_url }}" class="btn btn-sm btn-outline-secondary"
-                                            target="_blank">公式HP</a>
-                                    </div>
+    <div class="container">
+        <div class="border-bottom">
+            <h1 class="h2">Wi-Fi スポット</h1>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            @foreach ($wifispots as $wifispot)
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <h4>{{ $wifispot->name }}</h4>
+                        <img src="{{ $wifispot->image_url }}" alt="Sa" width="100%" height="225">
+                        <div class="card-body">
+                            <p class="card-text">{{ $wifispot->description }}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="{{ $wifispot->hp_url }}" class="btn btn-sm btn-outline-secondary"
+                                        target="_blank">公式HP</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
