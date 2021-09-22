d95af0dee5978066c8e5cab7ad92fb680b746506
YASUO0715 <yasuo0715niihori@gmail.com>
2021-09-23 08:05:55 +0900

    微調整HotSpring

--- resources/views/hot-spring/hotspring.blade.php
+++ resources/views/hot-spring/hotspring.blade.php
@@ -4,6 +4,7 @@
 @include('layout.header')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('./css/hot-spring.css') }}">
 
     <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
@@ -14,16 +15,16 @@
                         <img src="{{ $hotspring->image_url }}" alt="Sa" width="100%" height="225">
                         <div class="card-body">
                             <p class="card-text">{{ $hotspring->description }}</p>
+                            <div class="btn-group ">
+                            <p><a href="{{ $hotspring->hp_url }}" class="btn btn-sm btn-outline-secondary"
+                                            target="_blank">公式HP</a></p>
+                            </div>
                             <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <div class="map">
-                                        <iframe src="{{ $hotspring->map_url }}" width="400" height="300" style="border:0;"
-                                            allowfullscreen="" loading="lazy"></iframe>
-                                    </div>
-                                    <a href="{{ $hotspring->hp_url }}" class="btn btn-sm btn-outline-secondary"
-                                        target="_blank">公式HP</a>
-                                </div>
-
+                                <p><div class="map">
+                                    <iframe src="{{ $hotspring->map_url }}" width="330" height="300" style="border:0;"
+                                        allowfullscreen="" loading="lazy"></iframe>
+                                </div></p>
+                                
                             </div>
                         </div>
                     </div>
