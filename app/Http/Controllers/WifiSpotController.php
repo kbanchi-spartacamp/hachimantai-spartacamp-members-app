e33c619e5167cdedfd5d4f07f7b0b13d0a4f8e38
kbanchi1111 <kbanchi.1111@gmail.com>
2021-09-25 15:29:35 +0900

    update wifi spot call api

--- app/Http/Controllers/WifiSpotController.php
+++ app/Http/Controllers/WifiSpotController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\WifiSpot;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class WifiSpotController extends Controller
 {
     public function index()
     {
-        $wifispots = WifiSpot::all();
+        $method = 'GET';
+        $url = config('admin_api.url') . '/api/wifi-spots';
+        $options = [];
+
+        $client = new Client();
+        try {
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $wifispots = json_decode($body, false);
+        } catch (\Throwable $th) {
+            $wifispots = null;
+        }
+
         $data = [
             'wifispots' => $wifispots,
         ];
