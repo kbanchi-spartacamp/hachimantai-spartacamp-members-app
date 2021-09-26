f71c1ebe1a8742d562c8b8edaaf55f49e05a95a9
kbanchi1111 <kbanchi.1111@gmail.com>
2021-09-26 12:13:09 +0900

    update restaurants api

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class RestaurantController extends Controller
 {
     public function index()
     {
-        $restaurants = Restaurant::all();
+        $method = 'GET';
+        $url = config('admin_api.url') . '/api/restaurants';
+        $options = [];
+
+        $client = new Client();
+        try {
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $restaurants = json_decode($body, false);
+        } catch (\Throwable $th) {
+            $restaurants = null;
+        }
+
         $data = [
             'restaurants' => $restaurants,
         ];
